5c216996ffefa36ef0a1ea7e6a9f97d74138bf46
Acer <22103894@usc.edu.ph>
2026-03-12 15:54:51 +0800

    Update midterm project

--- app/src/main/java/com/cis2203n/exercise01/MainActivity.java
+++ app/src/main/java/com/cis2203n/exercise01/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    // update for midterm project
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
